86effb4feee5d3f0a15acee038100bac44984a0c
darren-ikinepule <darrenikinepule@outlook.co.nz>
2026-03-09 15:35:10 +1300

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
-import TrnNewCv from "./components/TrnNewCv";
-import SaleCv from "./components/SaleCv";
-=======
 
-
-
-
-
-
-
-
-import CustomerServiceDarrenCv from "./components/CustomerServiceDarrenCv";
-
-
->>>>>>> 806cff54906b1c0d44dbc4fbc40b5e4cbe68f5b8
+import ChefCv from "./components/ChefCv";
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-    <SaleCv />
-     <TrnNewCv />
-=======
-      <CustomerServiceDarrenCv/>
->>>>>>> 806cff54906b1c0d44dbc4fbc40b5e4cbe68f5b8
+     
+      <ChefCv />
     </>
   );
 };
